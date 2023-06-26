14e6ba0f298e61458168e3e93ac1bef3e1b42521
reivenIV <runyalen88@duck.com>
2023-06-26 17:11:15 +0200

    prettier

--- HourToUtcWithTimezone.js
+++ HourToUtcWithTimezone.js
@@ -4,18 +4,15 @@ const moment = require("moment-timezone");
 
 let payload = {
   created_at: "2023-06-26T13:16:44.000Z",
-  time_end: '18:00',
+  time_end: "18:00",
   timezone: "Europe/Paris",
 };
 
 let createdAtDate = moment(payload.created_at).format("YYYY-MM-DD");
 let timeEnd = payload.time_end;
 
-let datetime = createdAtDate + ' ' + timeEnd;
+let datetime = createdAtDate + " " + timeEnd;
 
-let utcTime = moment
-  .tz(datetime, payload.timezone)
-  .utc()
-  .format("HH:mm");
+let utcTime = moment.tz(datetime, payload.timezone).utc().format("HH:mm");
 
 console.log(utcTime);
